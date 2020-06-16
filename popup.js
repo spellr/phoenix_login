995e5e69f09ec8a6504b85ff4a0b566f1634eb45
Shoham Peller <shohamp@gmail.com>
2020-06-16 17:25:12 +0300

    Implemented the submit, which opens the final website

--- popup.js
+++ popup.js
@@ -1,27 +1,49 @@
+var token="";
+
 async function start() {
-	var user_id = "XXX"
-	var phone_number = "XXX"
+    var user_id = "XXX"
+    var phone_number = "XXX"
 
-	response = await fetch("https://myinfo.fnx.co.il/Fnx/MyZone/Registration/Registration", {credentials:'same-origin'});
-	text = await response.text();
-	parser = new DOMParser();
-	htmlDocument = parser.parseFromString(text, "text/html");
-	section = htmlDocument.documentElement.querySelector("[name=__RequestVerificationToken]");
-	token = section.getAttribute("value");
+    response = await fetch("https://myinfo.fnx.co.il/Fnx/MyZone/Registration/Registration", {credentials:'same-origin'});
+    text = await response.text();
+    parser = new DOMParser();
+    htmlDocument = parser.parseFromString(text, "text/html");
+    section = htmlDocument.documentElement.querySelector("[name=__RequestVerificationToken]");
+    token = section.getAttribute("value");
 
-	const regOpts = {
-	  method: 'POST',
-	  body: `__RequestVerificationToken=${token}&SelectedIdentityType=0&UserIdentity=${user_id}&CommissionedId=&SelectedOTPIdentificationType=0&UserNumberPhone=${phone_number}&UserNumberEmail=&g-recaptcha-response=&Consent=on&X-Requested-With=XMLHttpRequest`,
-	  headers: {
-	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-	  },
-	  credentials: 'same-origin',
-	};
-	await fetch("https://myinfo.fnx.co.il/fnx/MyZone/Registration/Registration/CheckValidateUser", regOpts);
+    const regOpts = {
+      method: 'POST',
+      body: `__RequestVerificationToken=${token}&SelectedIdentityType=0&UserIdentity=${user_id}&CommissionedId=&SelectedOTPIdentificationType=0&UserNumberPhone=${phone_number}&UserNumberEmail=&g-recaptcha-response=&Consent=on&X-Requested-With=XMLHttpRequest`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      credentials: 'same-origin',
+    };
+    await fetch("https://myinfo.fnx.co.il/fnx/MyZone/Registration/Registration/CheckValidateUser", regOpts);
 
-	window.open("https://myinfo.fnx.co.il/fnx/MyZone/Insurance/HomePage");
+    document.getElementById("isDisabled").removeAttribute("disabled");
+}
+
+async function submit() {
+    number = document.getElementById("smsNumber").value;
+    fetch("https://myinfo.fnx.co.il/fnx/MyZone/Registration/Registration/DoLogin", {
+        method: 'POST',
+          body: `__RequestVerificationToken=${token}&SecretCode=${number}`,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+          },
+          credentials: 'same-origin',
+          redirect: 'manual',
+    })
+    .then(function(resp) {
+        window.open("https://myinfo.fnx.co.il/fnx/MyZone/Insurance/HomePage");
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById("start").onclick = start;
+    document.getElementById("start").onclick = start;
+    document.getElementById("submit").onclick = submit;
 })
